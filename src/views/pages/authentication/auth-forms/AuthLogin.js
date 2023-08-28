8f7dedebca7ef6b155f48512852974744a6b9a67
Enow Divine <sirdivine16@gmail.com>
2023-08-28 19:11:56 +0100

    error fix

--- src/views/pages/authentication/auth-forms/AuthLogin.js
+++ src/views/pages/authentication/auth-forms/AuthLogin.js
@@ -152,7 +152,6 @@ const FirebaseLogin = ({ ...others }) => {
                 if (res.meta.requestStatus === 'rejected') {
                   setLoading(false);
                   toast.error(res.payload);
-                  toast.error(res.payload);
                 }
               })
               .catch((err) => {
